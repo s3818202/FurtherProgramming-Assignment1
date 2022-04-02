4f7b21bda09cd5d183dbab88d5f614532e0e7053
s3818202 <s3818202@rmit.edu.vn>
2022-04-02 18:57:15 +0700

    save change to csv function

--- Assignment1-building a console/src/main.java
+++ Assignment1-building a console/src/main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class main {
     public static void main(String[] args) {
         HandleEnroll h = new HandleEnroll();
-        h.display();         
+        h.display(); 
+        h.savetoCSVfile();        
     }
 }
